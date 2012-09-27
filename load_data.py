96ad6e7c067c4df886a83980b3d406fcee401023
Sean M. Collins <sean@coreitpro.com>
2012-09-27 17:02:33 -0400

    Add reverse mappings
    Create a User list and a reverse mapping from a User
    to the loans they have

--- load_data.py
+++ load_data.py
@@ -3,5 +3,16 @@ import redis
 import sys
 
 client = redis.Redis()
-map(lambda x: client.hmset("LOAN::%s" % x['Loan ID'], x),
-    csv.DictReader(open(sys.argv[1])))
+
+
+def process_row(row):
+    # Populate the Loan hash
+    client.hmset("Loan::%s" % row['Loan ID'], row)
+
+    # Add to the list of all user IDs
+    client.sadd("Users::", row['Screen Name'])
+
+    client.lpush("User::%s::Loans" % row['Screen Name'], row['Loan ID'])
+
+if __name__ == "__main__":
+    map(process_row, csv.DictReader(open(sys.argv[1])))
